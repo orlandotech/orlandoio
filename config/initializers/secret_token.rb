2c680f3d3ecd8b1f015038205ef845dc907eeec9
Orrett Davis <orrett.davis@gmail.com>
2013-12-11 14:47:07 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Orlandoio::Application.config.secret_key_base = 'a32fee7a3ef718aace47d5954b53d90682d16c3e3855a32653c91d4f8ffc803005eda1c5173cb6c215d67ee2065eedfcd192967693e1545ddc12ef9d36519797'
