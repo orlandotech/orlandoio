1b51795745e62dacf8f405a0bbb4a74f91815d5b
Orrett Davis <orrett.davis@gmail.com>
2013-12-16 19:54:40 -0500

    paginate order

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    @users = User.page params[:page]
+    @users = User.order(:created_at).page params[:page]
   # @users = User.order(:full_name)page params[:page]
   end
 
