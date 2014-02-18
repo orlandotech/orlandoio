acae2232e51cde3ab537bb1c6f3975078a5c4971
Orrett Davis <orrett.davis@gmail.com>
2014-02-17 23:36:56 -0500

    styling edits

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -5,9 +5,9 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     auth = env["omniauth.auth"]
     user = User.from_omniauth(env["omniauth.auth"], current_user)
     if user.persisted?
-      flash[:notice] = "Sign in - edit your profile to get listed on Orlando.io"
+      flash[:notice] = "Signed in successfully. Edit your profile to get listed on Orlando.io"
       identity = Identity.where(:provider => auth.provider, :uid => auth.uid, :token => auth.credentials.token, :secret => auth.credentials.secret).first_or_initialize
-      sign_in_and_redirect(user, identity.user)  
+      sign_in_and_redirect(user, identity.user)
     else
       session["devise.user_attributes"] = user.attributes
       redirect_to new_user_registration_url
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     published_users = User.joins(:profile).merge(Profile.published)
-    @users = published_users.limit(10).offset(rand(published_users.count))
+    @users = published_users.limit(9).offset(rand(published_users.count))
   end
 
   def about
