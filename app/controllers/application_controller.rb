3f5db316b5d168bc7562b4c366cabd4cad86fe24
Orrett Davis <orrett.davis@gmail.com>
2014-02-25 15:52:48 -0500

    fixed redirect stack error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,17 +14,14 @@ class ApplicationController < ActionController::Base
     session[:user_id] = user.nil? ? user : user.id
   end
 
-
-# edit to redirect normal signup to complete profile
+# redirect normal signup to complete profile
 def after_sign_in_path_for(resource)
-  # binding.pry
-  if current_user.profile.public == true
+  if @user.profile.public == true
     flash[:notice] = "Successfully signed in"
-    sign_in_and_redirect(@user)
+    profile_path(@user)
   else
-  flash[:danger] = "You must Complete your profile to get listed on Orlando.io"
-  # sign_in(@user)
-  redirect_to(current_user.profile)
+    flash[:danger] = "Complete your profile to get listed on Orlando.io"
+    edit_profile_path(@user)
   end
 end
 
