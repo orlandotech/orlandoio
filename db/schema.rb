5a9f57885988ba4bea6e46ecc454efbec33a2adc
Orrett Davis <orrett.davis@gmail.com>
2014-04-29 10:30:26 -0400

    updated act as taggable for rails 4.1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140227030503) do
+ActiveRecord::Schema.define(version: 20140429142956) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -107,7 +107,8 @@ ActiveRecord::Schema.define(version: 20140227030503) do
   add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
 
   create_table "tags", force: true do |t|
-    t.string "name"
+    t.string  "name"
+    t.integer "taggings_count", default: 0
   end
 
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true
